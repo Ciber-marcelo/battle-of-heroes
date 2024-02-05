1b7730c776e3d2345c42eb8d4cbc7de9301de6ed
Ciber-marcelo <marcelones125@gmail.com>
2024-02-05 20:53:03 -0300

    test: sass tests

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -2,17 +2,20 @@
 
 import axios from "axios"
 import { useEffect, useState } from "react";
+import styles from "./styles.module.css";
 
 export default function Section() {
-   const [character, setCharacter] = useState([]);
+   const [character, setCharacter] = useState('');
+   const [search, setSearch] = useState('');
 
-   useEffect(() => {
-      getCharacters()
-   }, [])
+   // useEffect(() => {
+   //    getCharacters()
+   // }, [])
 
-   async function getCharacters() {
+   async function getCharacter(char: any) {
       try {
-         const response = await axios.get("https://www.superheroapi.com/api.php/2450577805128827/search/hulk");
+         setCharacter('')
+         const response = await axios.get(`https://www.superheroapi.com/api.php/2450577805128827/search/${char}`);
          setCharacter(response.data.results[0].name)
          console.log('sucesso', response.data.results[0].name)
       } catch (error) {
@@ -21,6 +24,10 @@ export default function Section() {
    }
 
    return(
-      <div>teste 2s {character}</div>
+      <div className={styles.teta}>
+         <input onChange={(e: any) => setSearch(e.target.value)}></input>
+         <button onClick={() => getCharacter(search)}>button</button>
+         {character !== '' ? character : 'Nada...'}
+      </div>
    )
 }
