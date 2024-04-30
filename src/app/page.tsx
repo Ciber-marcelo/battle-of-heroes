8445de2f28fd220bfe9d57160197a9550b9ee369
Ciber-marcelo <marcelones125@gmail.com>
2024-04-29 22:15:00 -0300

    chore: edit section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Section from "@/components/Section";
 import { CharProvider } from "@/contexts/context";
 
@@ -5,7 +7,9 @@ export default function Home() {
   return (
     <div>
       <CharProvider>
+        <Header />
         <Section />
+        <Footer />
       </CharProvider>
     </div>
   );
