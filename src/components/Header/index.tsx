582f5b203ea1df28a279e937511e342ff188d65e
Ciber-marcelo <marcelones125@gmail.com>
2024-04-29 22:13:47 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export default function Header() {
+   return (
+      <div className={styles.container}>
+         <h1>BATTLE OF HEROES</h1>
+      </div>
+   )
+}
