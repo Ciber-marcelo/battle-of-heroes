63afa4e41c96d15f6cbdb3af59684a6200922b2f
Ciber-marcelo <marcelones125@gmail.com>
2024-06-15 17:48:51 -0300

    fix: fix responsive screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Section from "@/components/Section";
 import { CharProvider } from "@/contexts/context";
 
 export default function Home() {
   return (
-    <div>
+    <div className="vh">
       <CharProvider>
-        <Header />
         <Section />
-        <Footer />
       </CharProvider>
     </div>
   );
