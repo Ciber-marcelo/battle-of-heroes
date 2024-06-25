1c40e2645dba8396dfca891f52a18d733e02ae31
Ciber-marcelo <marcelones125@gmail.com>
2024-06-24 23:58:13 -0300

    chore: edit header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import styles from "./styles.module.css";
 
 export default function Header() {
    return (
-      <div className={styles.container}>
-         <h1>BATTLE OF HEROES</h1>
+      <div className={styles.main}>
+         <div className={styles.container}>
+            <Image
+               width={40}
+               height={40}
+               src={'/images/icon.png'}
+               alt="icon"
+            />
+            <h1 className={styles.hidden1}>BATTLE OF HEROES</h1>
+            {/* <h1 className={styles.hidden2}>HEROES</h1> */}
+         </div>
       </div>
    )
 }
