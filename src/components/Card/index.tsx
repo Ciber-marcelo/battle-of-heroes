c0cd947c08a7458f4e1e5ce8d58a3bae1ff0de6a
Ciber-marcelo <marcelones125@gmail.com>
2024-06-24 23:56:47 -0300

    chore: edit card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
 type cardProps = {
    image?: any
@@ -9,15 +10,36 @@ type cardProps = {
 }
 
 export default function Card({ image, name, realName, onClick }: cardProps) {
+   const [imgAdjust, setImgAdjust] = useState(image);
+   const [error, setError] = useState(false);
+
+   useEffect(() => {
+      setImgAdjust(image);
+      setError(false);
+    }, [image]);
+
+   const handleError = () => {
+      setError(true);
+      setImgAdjust('/images/none.png');
+    };
+  
+    const handleLoad = () => {
+      setError(false);
+    };
+
    return (
       <button className={styles.card} onClick={onClick}>
          <Image
             className={styles.image}
             width={180}
             height={240}
-            src={image}
+            src={imgAdjust}
             alt="image hero"
+            priority={true}
+            onError={handleError}
+            onLoad={handleLoad}
          />
+
          <div className={styles.text}>
             <p className={styles.t1}>{name}</p>
             <p className={styles.t2}>{realName}</p>
